fd7bcca03b3291d28086faa091d2050aeccf2612
AnikSinghal <aniksinghal2104@gmail.com>
2024-09-01 14:17:38 +0530

    final

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def logout():
 
 if __name__ == '__main__':
     app.run(
-        debug=True,
-            # host="0.0.0.0",
-            port=5000
+        # debug=True,
+            host="0.0.0.0"
+            # port=5000
             )
